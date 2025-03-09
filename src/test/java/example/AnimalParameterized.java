f2a95823a7db2ce41619430986bcf5552bf52ca1
Chvanova Kira Olegovna <kklivitair@gmail.com>
2025-03-09 16:32:11 +0300

    add jacoco report

---
+++ src/test/java/example/AnimalParameterized.java
@@ -0,0 +1,42 @@
+package example;
+
+import com.example.Animal;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Parameterized.class)
+public class AnimalParameterized {
+    private Animal animal;
+    private String animalKind;
+    private List<String> expectedFoods;
+
+    public AnimalParameterized(String animalKind, List<String> expectedFoods) {
+        this.animalKind = animalKind;
+        this.expectedFoods = expectedFoods;
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] getParameters() {
+        return new Object[][]{
+                {"Травоядное", List.of("Трава", "Различные растения")},
+                {"Хищник", List.of("Животные", "Птицы", "Рыба")}
+        };
+    }
+
+    @Before
+    public void setUp() {
+        animal = new Animal();
+    }
+
+    @Test
+    public void testGetFood() throws Exception {
+        List<String> actualFoods = animal.getFood(animalKind);
+        assertEquals("Список еды для животных не совпадает с ожидаемым", expectedFoods, actualFoods);
+    }
+}
